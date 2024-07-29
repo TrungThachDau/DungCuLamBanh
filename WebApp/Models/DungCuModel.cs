fea1602492415fbb8efed694aff27edbd3fb490d
android17x <qtrung1702@outlook.com>
2024-07-29 17:44:30 +0700

    commit

--- WebApp/Models/DungCuModel.cs
+++ WebApp/Models/DungCuModel.cs
@@ -1,5 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 namespace WebDungCuLamBanh.Models
 {
     [Table("SanPham", Schema = "dbo")]
